4e19749949d8fa2f68e09d64adf65097baa8a005
hrboan <119495992+hrboan@users.noreply.github.com>
2025-07-20 23:24:43 +0900

    Update and rename Week_Practice/Week_2/process_service_check.ps1 to Week_Practice/Week_2/PowerShell/process_service_check.ps1

--- Week_Practice/Week_2/process_service_check.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "`n[+] 현재 실행 중인 프로세스 목록"
-Get-Process | Sort-Object CPU -Descending | Select-Object Name, Id, CPU -First 10
-
-Write-Host "`n[+] 현재 실행 중인 서비스 목록"
-Get-Service | Where-Object { $_.Status -eq "Running" } | Select-Object Name, DisplayName
