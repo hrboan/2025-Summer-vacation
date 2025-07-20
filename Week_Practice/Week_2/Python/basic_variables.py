25b4106e76ef4ce8748769b6aa7de3c15a7e94d0
hrboan <119495992+hrboan@users.noreply.github.com>
2025-07-20 23:22:48 +0900

    Add files via upload

---
+++ Week_Practice/Week_2/Python/basic_variables.py
@@ -0,0 +1,10 @@
+name = "혜림"       # 문자열 (str)
+age = 24            # 정수 (int)
+height = 164      # 실수 (float)
+is_student = True   # 불리언 (bool)
+
+# 각 변수의 값과 자료형 출력
+print(f"name: {name}, type: {type(name)}")
+print(f"age: {age}, type: {type(age)}")
+print(f"height: {height}, type: {type(height)}")
+print(f"is_student: {is_student}, type: {type(is_student)}")
